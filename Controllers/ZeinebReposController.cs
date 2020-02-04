471d3e68e953ece3ba72e302ff1141c1d62e188f
zeineb.hassad@code-forefront.com <zeineb.hassad@code-forefront.com>
2020-02-04 11:49:20 +0100

    mon premier commit

---
+++ Controllers/ZeinebReposController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ExerciceLidlet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ZeinebReposController : ControllerBase
+    {
+        public HttpClient client;
+        public HttpContext rqt;
+        [HttpGet]
+        [EnableQuery]
+        public async Task<string>  Get()
+        {
+            var x = new ZeinebRepos();
+
+            return await x.getProjects(); 
+        }
+    }
+}
---
+++ ZeinebRepos.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExerciceLidlet
+{
+    public class ZeinebRepos
+    {
+
+        private readonly HttpContext request;
+        string BaseUrl = "https://api.lidlet.com/project";
+        public HttpClient client = new HttpClient();
+
+        public async Task<string> getProjects()
+        {
+            //string cookieValueFromContext = _httpContextAccessor.HttpContext.Request.Cookies["key"];
+            //string cookies = request.Request.Headers.Add("");
+            client.DefaultRequestHeaders.Add("cookie", "_ga=GA1.2.1396552231.1580301658; _gid=GA1.2.1498694230.1580301658; session=1195fa613a14b3ca55aed54e4b05e811");
+            HttpResponseMessage httpResponse = await client.GetAsync(BaseUrl);
+            string body = await httpResponse.Content.ReadAsStringAsync();
+            List<Projet> Projects = JsonConvert.DeserializeObject<List<Projet>>(body);
+            return Projects.First().name;
+    }
+}
+    }
+
+
