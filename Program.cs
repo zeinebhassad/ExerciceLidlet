151b232622860a1bb5be13e51230d94a7548fe0c
zeineb.hassad@code-forefront.com <zeineb.hassad@code-forefront.com>
2020-02-05 09:48:13 +0100

    Change port

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace ExerciceLidlet
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("https://localhost:5050") ;
                 });
     }
 }
